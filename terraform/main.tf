e71d3e6526c13a70cd7d25a2f08e498644f92266
SabariGokul11 <sabari.gokul.sys@gmail.com>
2026-04-03 13:38:52 +0530

    add one file

---
+++ terraform/main.tf
@@ -0,0 +1,51 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "game_server" {
+  ami           = "ami-05d2d839d4f73aafb" 
+  instance_type = "t3.small"
+  key_name      = "ubuntu"
+
+  security_groups = [aws_security_group.games]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install nginx -y
+              systemctl start nginx
+              systemctl enable nginx
+              EOF
+
+  tags = {
+    Name = "Game-Server"
+  }
+}
+
+resource "aws_security_group" "games" {
+  name = "games"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+output "public_ip" {
+  value = aws_instance.game_server.public_ip
+}
